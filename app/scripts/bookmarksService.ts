d309f305c4d219e088caa57d69e36afce52b4517
Ronny Ringel <ronnyr@wix.com>
2016-02-15 15:32:40 +0200

    services

--- app/scripts/bookmarksService.ts
+++ app/scripts/bookmarksService.ts
@@ -2,8 +2,47 @@
  * Created by ronnyr on 15/02/2016.
  */
 
-class BookmarksService {
-    constructor(){
-
-    }
+interface IBookmark {
+    id:number;
+    title:string;
+    creationDate:any;
 }
+
+class BookmarksService {
+    bookmarks:IBookmark[];
+    constructor() {
+        this.bookmarks = [
+            {
+                'id': 1,
+                'title': 'bookmark1',
+                'creationDate': '1455540591608'
+            },
+            {
+                'id': 2,
+                'title': 'bookmark2',
+                'creationDate': '1125540511608'
+            },
+            {
+                'id': 3,
+                'title': 'bookmark3',
+                'creationDate': '1452510591608'
+            }
+        ];
+    }
+
+    add = () => {
+        this.bookmarks.push(
+            {
+                'id': 4,
+                'title': 'bookmark4',
+                'creationDate': '1455978791608'
+            }
+        );
+    };
+
+    remove = (index:number) => this.bookmarks.splice(index, 1);
+}
+
+angular
+    .module('projectsApp')
+    .service('bookmarksService', BookmarksService);
--- app/scripts/mainCtrl.ts
+++ app/scripts/mainCtrl.ts
@@ -2,46 +2,23 @@
  * Created by ronnyr on 14/02/2016.
  */
 
-class Bookmark {
-    id:number;
-    title:string;
-    creationDate:any;
-}
+
 
 class MainController {
-    bookmarks:Bookmark[];
 
-    constructor() {
-        this.bookmarks = [
-            {
-                'id': 1,
-                'title': 'bookmark1',
-                'creationDate': '1455540591608'
-            },
-            {
-                'id': 2,
-                'title': 'bookmark2',
-                'creationDate': '1125540511608'
-            },
-            {
-                'id': 3,
-                'title': 'bookmark3',
-                'creationDate': '1452510591608'
-            }
-        ];
+
+    constructor(private bookmarksService:BookmarksService) {
     }
 
     addBookmark = () => {
-        this.bookmarks.push(
-            {
-                'id': 4,
-                'title': 'bookmark4',
-                'creationDate': '1455978791608'
-            }
-        );
+        this.bookmarksService.add();
     };
 
-    removeBookmark = (index:number) => this.bookmarks.splice(index, 1);
+    removeBookmark = (index:number) => this.bookmarksService.remove(index);
+
+    get bookmarks() {
+        return this.bookmarksService.bookmarks;
+    }
 }
 
 angular
